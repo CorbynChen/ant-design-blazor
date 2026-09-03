d2941af4cf198bbca23a1d7b08277a2b06b4ea26
CorbynChen <46276536+CorbynChen@users.noreply.github.com>
2026-09-03 16:21:38 +0800

    docs(tabs): clarify OnClosing XML documentation

--- components/tabs/TabPane.razor.cs
+++ components/tabs/TabPane.razor.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -62,7 +62,7 @@ namespace AntDesign
         public bool Closable { get; set; } = true;
 
         /// <summary>
-        /// Triggered before true closing, can prevent the closing
+        /// Callback executed before the tab is closed. The callback can cancel the closing operation.
         /// </summary>
         [Parameter]
         public EventCallback<CloseEventArgs<MouseEventArgs>> OnClosing { get; set; }
